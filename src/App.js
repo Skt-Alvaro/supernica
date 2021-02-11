3f41d0705703882796786dbca052e9f8dd246c48
Alvaro <65323913+Skt-Alvaro@users.noreply.github.com>
2021-02-10 21:49:28 -0600

    first commit, all works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
+import ShopInfo from "./components/ShopInfo";
+import generateStore from "./store/store";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Header from "./components/Header";
+import Store from "./components/Store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const store = generateStore();
+    return (
+        <Provider store={store}>
+            <BrowserRouter>
+                <Route exact path="/login" component={ShopInfo} />
+                <Route exact path="/store" component={Store} />
+                <Redirect from="/" to="/login" />
+            </BrowserRouter>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ src/components/ShopInfo.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+import people from "../images/gente.jpg";
+import Login from "../components/Login";
+import Header from "../components/Header";
+
+const P = styled.p`
+  font-size: 37px;
+  text-transform: uppercase;
+`;
+
+const P2 = styled.p`
+  font-size: 20px;
+  width: 82%;
+`;
+
+const Div = styled.div`
+  margin-top: 10%;
+  margin-left: 25px;
+
+  @media (max-width: 376px) {
+    margin-left: 10px;
+  }
+`;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 100%;
+  }
+
+  @media (min-height: 1367px) {
+    grid-template-columns: 100%;
+  }
+`;
+
+const Img = styled.img`
+  width: 91%;
+`;
+
+const ShopInfo = () => {
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Div>
+          <P>
+            Comprar nunca fue tan <strong>fácil</strong>
+          </P>
+          <br />
+          <P2>
+            Ahora puedes comprar tus productos sin salir de casa. Tienes un
+            control de tus gastos mensuales y muchas otras cosas más.
+          </P2>
+          <Img src={people} alt="" />
+        </Div>
+        <div>
+          <Login />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ShopInfo;
